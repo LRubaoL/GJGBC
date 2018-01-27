fa7f40a2f623d4cc749e52997aa44d7d6929c30f
Rubito <rubens.cesar.junior@hotmail.com>
2018-01-26 19:56:16 -0500

    Movement + Cam

---
+++ Assets/Scripts/Camera_Behaviour.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Camera_Behaviour : MonoBehaviour {
+
+	GameObject player;
+
+	float smoothSpeed = 0.4f;
+	public Vector3 offset;
+    Vector3 velocity;
+
+	void Awake(){
+        
+        
+
+    }
+	// Update is called once per frame
+	void FixedUpdate () {
+        player = GameObject.FindGameObjectWithTag("Player");
+
+
+        
+
+        Vector3 desiredPos = new Vector3(offset.x + player.transform.position.x, offset.y + player.transform.position.y, offset.z);
+        Vector3 smoothedPos = Vector3.SmoothDamp (transform.position, desiredPos, ref velocity ,smoothSpeed);
+        transform.position = smoothedPos;
+
+
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerMovement : MonoBehaviour {
+    
+	[SerializeField] float rotateSpeed;
+    [SerializeField] float jumpForce;
+    [SerializeField] float moveSpeed;
+    [SerializeField] float doubleJumpForce;
+    
+    
+    public bool grounded = false;
+    bool jump = false;
+    bool doubleJump = false;
+
+    Rigidbody rbd;
+
+    
+
+	Camera playerCam;
+
+
+    void Start() {
+		playerCam = GameObject.FindGameObjectWithTag ("SceneCamera").GetComponent<Camera>();
+
+        rbd = GetComponent<Rigidbody>();
+    }
+
+    void Update() {
+		if (grounded) {
+			jump = true;
+
+		}	
+
+		if (Input.GetButtonDown("Jump")&& grounded ||Input.GetKeyDown(KeyCode.W) && grounded){
+			Jump();
+            print ("Jump");
+        }
+		if (Input.GetButtonDown ("Jump") && !grounded && doubleJump) { //&& isKnockback == false) {
+			DoubleJump();
+
+        }
+    }
+	void OnCollisionStay(Collision c){
+		if (c.gameObject.tag == "Ground") {
+			grounded = true;
+		} 
+	}
+	void OnCollisionExit(Collision c){
+		if (c.gameObject.tag == "Ground" || c.gameObject.tag == "Player") {
+		grounded = false;
+		}
+	}
+
+	void FixedUpdate() {
+            //Movement Translation
+            float h = Input.GetAxis("Horizontal") * moveSpeed;
+            h *= Time.deltaTime;
+            transform.Translate(h, 0, 0, Space.World);
+			
+			Aim();        
+    }
+
+    void Aim() {
+        if (!grounded) {
+
+            Vector3 mousePosition = Input.mousePosition;
+            mousePosition.z = 5;
+            Vector3 objectPos = playerCam.WorldToScreenPoint(transform.position);
+            mousePosition.x = mousePosition.x - objectPos.x;
+            mousePosition.y = mousePosition.y - objectPos.y;
+			float angle = Mathf.Atan2(mousePosition.y, mousePosition.x) * Mathf.Rad2Deg;
+	        //transform.rotation = Quaternion.Euler(0, 0, angle);
+			Quaternion _rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+			transform.rotation = Quaternion.Slerp (transform.rotation, _rotation, rotateSpeed*Time.deltaTime);
+        }
+    }
+
+void Jump() {
+		if (jump) {
+			rbd.AddForce (new Vector2 (0, jumpForce));
+			jump = false;
+			doubleJump = true;
+		}
+	}
+		void DoubleJump(){
+        if (doubleJump) {
+            rbd.AddForce(new Vector2(0, doubleJumpForce));
+            doubleJump = false;
+           
+	}
+    }
+
+}
